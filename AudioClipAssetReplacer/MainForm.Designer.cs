4ce01c70e2cf81a7f6715f43555e85b6cf04dbc3
Fallen_Angle <satomatsuzaka8@gmail.com>
2024-03-19 10:47:31 +0300

    Create

--- AudioClipAssetReplacer/MainForm.Designer.cs
+++ AudioClipAssetReplacer/MainForm.Designer.cs
@@ -28,12 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            openAssetsDialog = new OpenFileDialog();
+            openFsbDialog = new OpenFileDialog();
+            saveAssetsDialog = new SaveFileDialog();
+            audioGroupBox = new GroupBox();
+            audioGridView = new DataGridView();
+            splitContainer1 = new SplitContainer();
+            replaceButton = new Button();
+            linkButton = new Button();
+            saveResourceDialog = new SaveFileDialog();
+            menuStrip1.SuspendLayout();
+            audioGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)audioGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(767, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, saveAsToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(224, 26);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(224, 26);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(224, 26);
+            saveAsToolStripMenuItem.Text = "Save As...";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(224, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // openAssetsDialog
+            // 
+            openAssetsDialog.DefaultExt = "assets";
+            openAssetsDialog.Filter = "Assets file|*.assets";
+            // 
+            // openFsbDialog
+            // 
+            openFsbDialog.DefaultExt = "fsb";
+            openFsbDialog.Filter = "FSB file|*.fsb";
+            // 
+            // saveAssetsDialog
+            // 
+            saveAssetsDialog.DefaultExt = "assets";
+            saveAssetsDialog.Filter = "Assets file|*.assets";
+            // 
+            // audioGroupBox
+            // 
+            audioGroupBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            audioGroupBox.Controls.Add(audioGridView);
+            audioGroupBox.Location = new Point(12, 31);
+            audioGroupBox.Name = "audioGroupBox";
+            audioGroupBox.Size = new Size(743, 352);
+            audioGroupBox.TabIndex = 1;
+            audioGroupBox.TabStop = false;
+            audioGroupBox.Text = "Select your audio asset";
+            // 
+            // audioGridView
+            // 
+            audioGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            audioGridView.Dock = DockStyle.Fill;
+            audioGridView.Location = new Point(3, 23);
+            audioGridView.Name = "audioGridView";
+            audioGridView.RowHeadersWidth = 51;
+            audioGridView.RowTemplate.Height = 29;
+            audioGridView.Size = new Size(737, 326);
+            audioGridView.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(12, 389);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(replaceButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(linkButton);
+            splitContainer1.Size = new Size(743, 54);
+            splitContainer1.SplitterDistance = 370;
+            splitContainer1.TabIndex = 2;
+            // 
+            // replaceButton
+            // 
+            replaceButton.Dock = DockStyle.Fill;
+            replaceButton.Location = new Point(0, 0);
+            replaceButton.Name = "replaceButton";
+            replaceButton.Size = new Size(370, 54);
+            replaceButton.TabIndex = 0;
+            replaceButton.Text = "Replace audio";
+            replaceButton.UseVisualStyleBackColor = true;
+            replaceButton.Click += replaceButton_Click;
+            // 
+            // linkButton
+            // 
+            linkButton.Dock = DockStyle.Fill;
+            linkButton.Location = new Point(0, 0);
+            linkButton.Name = "linkButton";
+            linkButton.Size = new Size(369, 54);
+            linkButton.TabIndex = 0;
+            linkButton.Text = "Link audio";
+            linkButton.UseVisualStyleBackColor = true;
+            linkButton.Click += linkButton_Click;
+            // 
+            // saveResourceDialog
+            // 
+            saveResourceDialog.DefaultExt = "resource";
+            saveResourceDialog.Filter = "Resource file|*.resource";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(767, 455);
+            Controls.Add(splitContainer1);
+            Controls.Add(audioGroupBox);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            ShowIcon = false;
+            Text = "AudioClipAssetReplacer";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            audioGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)audioGridView).EndInit();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private OpenFileDialog openAssetsDialog;
+        private OpenFileDialog openFsbDialog;
+        private SaveFileDialog saveAssetsDialog;
+        private GroupBox audioGroupBox;
+        private DataGridView audioGridView;
+        private SplitContainer splitContainer1;
+        private Button replaceButton;
+        private Button linkButton;
+        private SaveFileDialog saveResourceDialog;
     }
 }
--- AudioClipAssetReplacer/MainForm.cs
+++ AudioClipAssetReplacer/MainForm.cs
@@ -1,10 +1,228 @@
+using AssetsTools.NET.Extra;
+using AssetsTools.NET;
+
 namespace AudioClipAssetReplacer
 {
     public partial class MainForm : Form
     {
+        const string ERROR_TITLE = "Error";
+        AssetsManager manager = new AssetsManager();
+        AssetsFileInstance fileInstance;
+        AssetsFile assetsFile;
+        string assetsPath;
+        byte[] fsbFileData;
+        string resourcePath;
+        byte[] resourceFile;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openAssetsDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                assetsPath = openAssetsDialog.FileName;
+                LoadAssetsFile(assetsPath);
+            }
+        }
+
+        private void LoadAssetsFile(string assetsPath)
+        {
+            audioGridView.Rows.Clear();
+            manager.LoadClassPackage("classdata.tpk");
+            fileInstance = manager.LoadAssetsFile(assetsPath, loadDeps: true);
+            assetsFile = fileInstance.file;
+            manager.LoadClassDatabaseFromPackage(assetsFile.Metadata.UnityVersion);
+            UpdateAssetsFileList();
+        }
+
+        private void UpdateAssetsFileList()
+        {
+            if (assetsFile != null)
+            {
+                int savedSelectedRowIndex = 0;
+                if (audioGridView.SelectedRows.Count != 0)
+                {
+                    savedSelectedRowIndex = audioGridView.SelectedRows[0].Index;
+                }
+                audioGridView.Rows.Clear();
+                List<AssetFileInfo> audioInfos = assetsFile.GetAssetsOfType(AssetClassID.AudioClip);
+                for (int i = 0; i < audioInfos.Count; i++)
+                {
+                    AssetFileInfo audioInfo = audioInfos[i];
+                    AssetTypeValueField audioBase = manager.GetBaseField(fileInstance, audioInfo);
+                    audioGridView.Rows.Add();
+                    audioGridView.Rows[i].Cells[0].Value = audioBase["m_Name"].AsString;
+                    audioGridView.Rows[i].Cells[1].Value = audioInfo.PathId;
+                    audioGridView.Rows[i].Cells[2].Value = audioBase["m_Resource.m_Offset"].AsULong;
+                    audioGridView.Rows[i].Cells[3].Value = audioBase["m_Resource.m_Size"].AsULong;
+                    audioGridView.Rows[i].Cells[4].Value = audioBase["m_Resource.m_Source"].AsString;
+                }
+                if (audioGridView.Rows.Count != 0)
+                {
+                    audioGridView.Rows[savedSelectedRowIndex].Selected = true;
+                }
+            }
+        }
+
+        private byte[] GetFsbData(long pathID)
+        {
+            AssetTypeValueField audioBase = manager.GetBaseField(fileInstance, pathID);
+            string resourceName = audioBase["m_Resource.m_Source"].AsString;
+            ulong audioOffset = audioBase["m_Resource.m_Offset"].AsULong;
+            ulong dataSize = audioBase["m_Resource.m_Size"].AsULong;
+            string currentDir = GetDirectory(assetsPath);
+            string resourcePath = currentDir + resourceName;
+            if (!File.Exists(resourcePath))
+            {
+                MessageBox.Show("The resource file (" + resourceName + ") doesn't exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return new byte[0];
+            }
+            byte[] resourceBytes = File.ReadAllBytes(resourcePath);
+            MemoryStream memoryStream = new MemoryStream(resourceBytes);
+            BinaryReader binaryReader = new BinaryReader(memoryStream);
+            binaryReader.BaseStream.Position = Convert.ToInt64(audioOffset);
+            return binaryReader.ReadBytes(Convert.ToInt32(dataSize));
+        }
+
+        private void LinkFsbData(long pathID, string fsbPath, long fsbSize)
+        {
+            fsbPath = fsbPath.Replace("\\", "/");
+            AssetTypeValueField audioBase = manager.GetBaseField(fileInstance, pathID);
+            AssetFileInfo audioInfo = assetsFile.GetAssetInfo(pathID);
+            audioBase["m_Resource.m_Source"].AsString = fsbPath;
+            audioBase["m_Resource.m_Offset"].AsULong = 0uL;
+            audioBase["m_Resource.m_Size"].AsULong = Convert.ToUInt64(fsbSize);
+            audioInfo.SetNewData(audioBase);
+        }
+
+        private void ReplaceFsbData(long pathID)
+        {
+            AssetTypeValueField audioBase = manager.GetBaseField(fileInstance, pathID);
+            AssetFileInfo audioInfo = assetsFile.GetAssetInfo(pathID);
+            string resourceName = audioBase["m_Resource.m_Source"].AsString;
+            ulong audioOffset = audioBase["m_Resource.m_Offset"].AsULong;
+            ulong dataSize = audioBase["m_Resource.m_Size"].AsULong;
+            string currentDir = GetDirectory(assetsPath);
+            resourcePath = currentDir + resourceName;
+            if (!File.Exists(resourcePath))
+            {
+                MessageBox.Show("The resource file (" + resourceName + ") doesn't exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+            List<byte> resourceBytes = File.ReadAllBytes(resourcePath).ToList();
+            resourceBytes.RemoveRange(Convert.ToInt32(audioOffset), Convert.ToInt32(dataSize));
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+            binaryWriter.Write(resourceBytes.ToArray());
+            File.WriteAllBytes(".\\Testing.resource", memoryStream.ToArray());
+            MessageBox.Show("Done");
+            binaryWriter.BaseStream.Position = Convert.ToInt64(audioOffset);
+            binaryWriter.Write(fsbFileData);
+            resourceFile = memoryStream.ToArray();
+            audioBase["m_Resource.m_Size"].AsULong = Convert.ToUInt64(fsbFileData.LongLength);
+            audioInfo.SetNewData(audioBase);
+            for (int i = 0; i < audioGridView.Rows.Count; i++)
+            {
+                ulong currentAudioOffset = Convert.ToUInt64(audioGridView.Rows[i].Cells[2].Value);
+                if (currentAudioOffset > audioOffset)
+                {
+                    long currentPathID = Convert.ToInt64(audioGridView.Rows[i].Cells[1].Value);
+                    AssetTypeValueField currentAudioBase = manager.GetBaseField(fileInstance, currentPathID);
+                    AssetFileInfo currentAudioInfo = assetsFile.GetAssetInfo(currentPathID);
+                    long offsetAfterWriting = fsbFileData.LongLength - Convert.ToInt64(dataSize);
+                    if (offsetAfterWriting > 0)
+                    {
+                        currentAudioBase["m_Resource.m_Offset"].AsULong += Convert.ToUInt64(offsetAfterWriting);
+                    }
+                    else
+                    {
+                        offsetAfterWriting *= -1;
+                        currentAudioBase["m_Resource.m_Offset"].AsULong -= Convert.ToUInt64(offsetAfterWriting);
+                    }
+                    currentAudioInfo.SetNewData(currentAudioBase);
+                }
+            }
+        }
+
+        private void SaveAssetsFile(AssetsFile assetsFile, string filePath)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using AssetsFileWriter writer = new AssetsFileWriter(memoryStream);
+            assetsFile.Write(writer, 0L);
+            assetsFile.Close();
+            using (FileStream fs = File.OpenWrite(filePath))
+            {
+                memoryStream.WriteTo(fs);
+            }
+            assetsFile.Read(new AssetsFileReader(filePath));
+        }
+
+        private string GetDirectory(string filePath)
+        {
+            return filePath.Replace(Path.GetFileName(filePath), string.Empty);
+        }
+
+        private void replaceButton_Click(object sender, EventArgs e)
+        {
+            if (audioGridView.Rows.Count != 0 && openFsbDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                fsbFileData = File.ReadAllBytes(openFsbDialog.FileName);
+                long pathID = Convert.ToInt64(audioGridView.SelectedRows[0].Cells[1].Value);
+                ReplaceFsbData(pathID);
+                UpdateAssetsFileList();
+            }
+        }
+
+        private void linkButton_Click(object sender, EventArgs e)
+        {
+            if (audioGridView.Rows.Count != 0 && openFsbDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                string mainDir = GetDirectory(assetsPath);
+                if (!openFsbDialog.FileName.StartsWith(mainDir))
+                {
+                    MessageBox.Show("Your file must be located inside a folder of the assets file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    return;
+                }
+                string relFsbPath = openFsbDialog.FileName.Replace(mainDir, string.Empty);
+                long pathID = Convert.ToInt64(audioGridView.SelectedRows[0].Cells[1].Value);
+                long size = new FileInfo(openFsbDialog.FileName).Length;
+                LinkFsbData(pathID, relFsbPath, size);
+                UpdateAssetsFileList();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAssetsFile(assetsFile, assetsPath);
+            if (resourceFile != null)
+            {
+                File.WriteAllBytes(resourcePath, resourceFile);
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveAssetsDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            if (resourceFile != null)
+            {
+                if (saveResourceDialog.ShowDialog() == DialogResult.Cancel)
+                {
+                    return;
+                }
+                File.WriteAllBytes(saveResourceDialog.FileName, resourceFile);
+            }
+            SaveAssetsFile(assetsFile, saveAssetsDialog.FileName);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
